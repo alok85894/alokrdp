69e147baf8dc360bbda7981cfbd2b4692328ed08
jajananenak <81418435+jajananenak@users.noreply.github.com>
2021-03-27 19:26:40 -0700

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -1,7 +1,11 @@
 sudo apt update
 sudo apt -y install screen
 sudo apt -y install xfce4
+sudo apt -y install xfce4-goodies
 sudo apt -y install firefox
+sudo apt -y install obs-studio
+sudo apt -y  xarchiver
+sudo apt -y vlc
 sudo apt install tightvncserver
 tightvncserver :1
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
